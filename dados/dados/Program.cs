334529c2deecab8d052218cfe8645fe5adf7da08
ronald0309 <ronaldba16@gmail.com>
2019-12-02 23:02:56 -0600

    se crean los numeros random para el dado

--- dados/dados/Program.cs
+++ dados/dados/Program.cs
@@ -38,6 +38,24 @@ namespace dados
             Console.WriteLine("             #      #");
             Console.WriteLine("             ########");
         }
+        //funcion para generar los valores aleatorios del dado 
+        static void randomDado()
+        {
+            Random random = new Random();
+            int x;
+            int x2;
+            int x3;
+            x = random.Next(1, 7);
+            do
+            {
+                x2 = random.Next(1, 7);
+            } while (x == x2 || x == (7 - x2));
+            do
+            {
+                x3 = random.Next(1, 7);
+            } while (x == x3 || x2 == x3|| (7-x) == x3|| (7-x2) == x3);
+            Console.WriteLine("Valores del random {0},{1},{2}", x, x2, x3);
+        }
         static void Main(string[] args)
         {
             // Se valida si se preciona un tecla diferente a la tecla escape 
@@ -47,7 +65,10 @@ namespace dados
                 while ( Console.ReadKey().Key == ConsoleKey.Enter)
                 {
                     Console.WriteLine("Nueva Visualizacion");
+                    // se valida que el random funcione 
+                    randomDado();
                     // Se imprime el dado.
+
                     imprimirDado(1, 2, 3);
                 }
             }
